3b0d490b73a3d40ecba11178166519f1b9368762
johnwiesenfeld <jawiesenfeld@gmail.com>
2019-05-10 13:37:02 -0700

    first pass messing around

--- Driver.java
+++ Driver.java
@@ -1,5 +1,24 @@
-// COMP282 Section 16304 Project 3
+// COMP282 Section 16304 Project 4
 // Group members:
 // Nicholas Warfield
 // Javier Aguayo
 // John Wiesenfeld
+import java.io.File;
+import java.util.Scanner;
+
+public class Driver{
+
+	public Graph graph;
+
+	public static void main(String[] args)
+	{
+		Driver driver = new Driver();
+		driver.execute;
+	}
+
+	public void execute()
+	{
+		Scanner sc = new Scanner(System.in);
+
+	}
+}
